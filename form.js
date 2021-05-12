34a16155d8b30351bd9ea3e5ff1aecbb0cb441fb
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-12 12:11:41 -0400

    keyup event for input validation

--- form.js
+++ form.js
@@ -16,3 +16,16 @@ form.addEventListener('submit', (e)=>{
    // displayName.innerText=form.username.value 
 
 })
+
+form.username.addEventListener('keyup', e=>{
+    if (pattern.test(e.target.value)){
+        //this can be done in two ways
+        //1
+        form.username.setAttribute('class', 'success')
+        //2
+        //form.username.classList.add('success')
+    } else{
+        form.username.setAttribute('class', 'error')
+        //form.username.classList.add('error')
+    }
+})
